58a646560e62f07ffdaf9269906255247252c939
Javier Parejo jimenez <jparejo-@c2r4s5.42madrid.com>
2021-10-07 14:18:06 +0200

    libft

---
+++ ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jparejo- <jparejo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 10:27:55 by jparejo-          #+#    #+#             */
+/*   Updated: 2021/10/06 13:00:11 by jparejo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	if (!(start > ft_strlen(s)))
+	{
+		while (i < len)
+		{
+			str[i] = s[start + i];
+			i++;
+		}
+	}
+	str[i] = '\0';
+	return (str);
+}
